3c50a6c7761d470bfc7e523f8917fbf74c5f206e
ONABOTS <onabots5534@gmail.com>
2026-01-06 16:13:11 -0500

    Update reefscape_config.js aaded folder ref

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -97,7 +97,7 @@ var config_data = `
     { "name": "Auto Scoring Position",
       "code": "asp",
       "type": "clickable_image",
-      "filename": "American-style_pool_table_diagram_(empty).png",
+      "filename": "2025\American-style_pool_table_diagram_(empty).png",
       "dimensions": "6 6",
       "allowableResponses": "1 2 3 4 5 6 7 8 9 10 11 12 13 14 17 18 19 20 23 24 25 26 27 28 29 30 31 32 33 34 35 36",
       "shape": "circle 5 black red true"
